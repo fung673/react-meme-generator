84eb6662f0a6de36195ce59bbc69d16f3f7f4b51
unknown <alexfung1234@gmail.com>
2022-11-01 20:41:39 +0800

    update build for gh-pages

--- src/component/meme.js
+++ src/component/meme.js
@@ -5,13 +5,14 @@ import { useState } from "react";
 
 export default function Meme() {
 
- const [oldurl, newurl] = useState("")
+ const [meme, setMeme] = useState({ topText: "", bottomText: "", randomImage: "http://i.imgflip.com/1bij.jpg" })
+ const [allMemeImages, setAllMemeImages] = useState(memedata)
 
  function getMemeImage() {
-  const memesarray = memedata.data.memes
+  const memesarray = allMemeImages.data.memes
   const randomnumber = Math.floor(Math.random() * memesarray.length)
   const randomurl = memesarray[randomnumber].url
-  newurl(randomurl)
+  setMeme(prevMeme => ({ ...prevMeme, randomImage: randomurl }))
 
  }
 
@@ -21,7 +22,7 @@ export default function Meme() {
   <div class="break"></div>
   <button className="button" onClick={getMemeImage} >Get a new meme image  🖼️</button>
   <div class="break"></div>
-  <img src={oldurl} className="meme--image" />
+  <img src={meme.randomImage} className="meme--image" />
  </div>)
 
 }
